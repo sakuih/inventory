ef259d6ed392a5413e0212aff4ff91e5fc850804
taco <taco@taco.taco>
2023-11-13 00:25:26 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+dotenv.config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get()
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`)) 
+
+
